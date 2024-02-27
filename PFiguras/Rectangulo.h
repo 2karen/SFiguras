5d12f82576af771b4ae421fdc7fe3d06600eff50
karen <karen@KAREN>
2024-02-27 15:44:45 -0400

    Agregar archivos de proyecto.

---
+++ PFiguras/Rectangulo.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Figuras.h"
+#include "Cuadrado.h"
+class Rectangulo : public Cuadrado
+{
+private:
+	//se abstrae la figura en alto y ancho
+	float alto, ancho;
+public:
+
+	Rectangulo(float _alto, float _ancho);
+	void calcularAreaPerimetro();
+};
+
